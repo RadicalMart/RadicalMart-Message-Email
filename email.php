0d04e7562d1ea59a8bc1b85030a3b9a7b7bc1d32
Igor Berdichevskiy <iberdichevskiy@septdir.com>
2022-03-01 16:18:40 +0300

    Change version

--- email.php
+++ email.php
@@ -2,7 +2,7 @@
 /*
  * @package     RadicalMart Package
  * @subpackage  plg_radicalmart_message_email
- * @version     __DEPLOY_VERSION__
+ * @version     1.1.0
  * @author      Delo Design - delo-design.ru
  * @copyright   Copyright (c) 2021 Delo Design. All rights reserved.
  * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
@@ -47,7 +47,7 @@ class plgRadicalMart_MessageEmail extends CMSPlugin
 	 *
 	 * @throws  Exception
 	 *
-	 * @since  __DEPLOY_VERSION__
+	 * @since  1.1.0
 	 */
 	public function onRadicalMartPrepareConfigForm($form, $data)
 	{
@@ -63,7 +63,7 @@ class plgRadicalMart_MessageEmail extends CMSPlugin
 	 *
 	 * @throws  Exception
 	 *
-	 * @since  __DEPLOY_VERSION__
+	 * @since  1.1.0
 	 */
 	public function onRadicalMartExpressPrepareConfigForm($form, $data)
 	{
