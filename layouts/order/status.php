d4d0c38bd16be136477b96d033bb5b130cb5d6f6
Igor Berdichevskiy <iberdichevskiy@septdir.com>
2022-08-15 13:18:43 +0300

    Reset to dev

--- layouts/order/status.php
+++ layouts/order/status.php
@@ -2,7 +2,7 @@
 /*
  * @package     RadicalMart Package
  * @subpackage  plg_radicalmart_message_email
- * @version     1.1.2
+ * @version     __DEPLOY_VERSION__
  * @author      Delo Design - delo-design.ru
  * @copyright   Copyright (c) 2021 Delo Design. All rights reserved.
  * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
