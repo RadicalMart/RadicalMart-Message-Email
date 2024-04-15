da536f0a873b7a6756ef996bf490219dbb2d7b73
Igor Berdichevskiy <septdir@gmail.com>
2024-04-15 13:21:42 +0300

    Update copyrights

--- layouts/radicalmart_express/order/create.php
+++ layouts/radicalmart_express/order/create.php
@@ -1,12 +1,12 @@
 <?php
 /*
- * @package     RadicalMart Package
+ * @package     RadicalMart Messages Email Plugin
  * @subpackage  plg_radicalmart_message_email
  * @version     __DEPLOY_VERSION__
- * @author      Delo Design - delo-design.ru
- * @copyright   Copyright (c) 2023 Delo Design. All rights reserved.
+ * @author      RadicalMart Team - radicalmart.ru
+ * @copyright   Copyright (c) 2024 RadicalMart. All rights reserved.
  * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
- * @link        https://delo-design.ru/
+ * @link        https://radicalmart.ru/
  */
 
 defined('_JEXEC') or die;
